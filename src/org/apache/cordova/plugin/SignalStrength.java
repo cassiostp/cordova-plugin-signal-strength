1e373dac75fe15c646f3890e7244435253e584e2
Cássio Terceiro <cassio.terceiro@gmail.com>
2018-12-05 17:09:04 -0300

    Change logic to give me all possible signalStrengths

--- src/org/apache/cordova/plugin/SignalStrength.java
+++ src/org/apache/cordova/plugin/SignalStrength.java
@@ -1,56 +1,62 @@
-package org.apache.cordova.plugin;
+package org.apache.cordova.signalStrength;
 
 import android.content.Context;
 
-import android.telephony.PhoneStateListener;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.TelephonyManager;
+import android.util.ArrayMap;
+import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Map;
 
 public class SignalStrength extends CordovaPlugin {
 
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("dbm")) {
-                ssListener = new SignalStrengthStateListener();
-                TelephonyManager tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-                tm.listen(ssListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-                int counter = 0;
-                while ( dbm == -1) {
-                        try {
-                                Thread.sleep(200);
-                        } catch (InterruptedException e) {
-                                e.printStackTrace();
-                        }
-                        if (counter++ >= 5)
-                        {
-                                break;
-                        }
+            TelephonyManager tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+            List<CellInfo> cellInfoList = tm.getAllCellInfo();
+            Map<String, Integer> allDbm = new ArrayMap();
+            if (cellInfoList != null) {
+                for (final CellInfo info : cellInfoList) {
+                    if (info instanceof CellInfoCdma) {
+                        final CellSignalStrengthCdma cdma = ((CellInfoCdma) info).getCellSignalStrength();
+                        allDbm.put("cdma", cdma.getDbm());
+                    } else if (info instanceof CellInfoGsm) {
+                        final CellSignalStrengthGsm gsm = ((CellInfoGsm) info).getCellSignalStrength();
+                        allDbm.put("gsm", gsm.getDbm());
+                    } else if (info instanceof CellInfoWcdma) {
+                        final CellSignalStrengthWcdma wcdma = ((CellInfoWcdma) info).getCellSignalStrength();
+                        allDbm.put("wcdma", wcdma.getDbm());
+                    } else if (info instanceof CellInfoLte) {
+                        final CellSignalStrengthLte lte = ((CellInfoLte) info).getCellSignalStrength();
+                        allDbm.put("lte", lte.getDbm());
+                    }
                 }
-                callbackContext.success(dbm);
+            }
+            if (allDbm.size() > 0) {
+                Log.d("SIGNAL-STRENGTHS", allDbm.toString());
+                callbackContext.success((JSONArray) allDbm);
                 return true;
+            }
+            return false;
         }
-
         return false;
-}
-
-
-class SignalStrengthStateListener extends PhoneStateListener {
-
-@Override
-public void onSignalStrengthsChanged(android.telephony.SignalStrength signalStrength) {
-        super.onSignalStrengthsChanged(signalStrength);
-        int tsNormSignalStrength = signalStrength.getGsmSignalStrength();
-        dbm = (2 * tsNormSignalStrength) - 113;     // -> dBm
-}
-
-}
-
-SignalStrengthStateListener ssListener;
-int dbm = -1;
+    }
 
 }
